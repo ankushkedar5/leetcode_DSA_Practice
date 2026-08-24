94bbcd794f194ba0ecf46396294de54f7fd9cc2c
Ankush Kedar <ankushkedar2007@gmail.com>
2026-08-24 17:41:43 +0530

    Time: 0 ms (100%), Space: 43.2 MB (79.65%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         for(int i=digits.length-1; i>=0; i--) {
-            if(digits[i] != 9) {
+            if(digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
