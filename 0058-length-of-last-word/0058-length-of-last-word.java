040c778291900f44ce44b8861b557cba088bd433
Ankush Kedar <ankushkedar2007@gmail.com>
2026-08-24 17:04:58 +0530

    Time: 1 ms (27.68%), Space: 43.1 MB (32.77%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] words = s.split(" ");
-        int lastWordIdx = words.length-1;
-        return words[lastWordIdx].length();
+        return words[words.length-1].length();
     }
 }
