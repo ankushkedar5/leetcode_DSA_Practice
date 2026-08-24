784b50cf9cb232e116905baf8b1713b89f0d0f61
Ankush Kedar <ankushkedar2007@gmail.com>
2026-08-24 17:40:21 +0530

    Time: 0 ms (100%), Space: 43.4 MB (61.93%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -5,12 +5,10 @@ class Solution {
                 digits[i]++;
                 return digits;
             }
-            else {
-                digits[i] = 0;
-            }
+            digits[i] = 0;
         }
-        int[] result = new int[digits.length+1];
-        result[0] = 1;
-        return result;
+        int[] arr = new int[digits.length+1];
+        arr[0] = 1;
+        return arr;
     }
 }
