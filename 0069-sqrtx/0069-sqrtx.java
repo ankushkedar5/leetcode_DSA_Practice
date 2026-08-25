ad61c15a1974c63279b88238fadb4cb4e64e1077
Ankush Kedar <ankushkedar2007@gmail.com>
2026-08-25 18:54:04 +0530

    Time: 1 ms (98.47%), Space: 42.7 MB (23.6%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -7,11 +7,10 @@ class Solution {
         int ans = 0;
         while(st <= ls) {
             int md = st + (ls - st) / 2;
-            long sq = (long) md * md;
-            if(sq == x) {
+            if((long) md*md == x) {
                 return md;
             }
-            else if(sq < x) {
+            else if((long) md*md < x) {
                 ans = md;
                 st = md+1;
             }
