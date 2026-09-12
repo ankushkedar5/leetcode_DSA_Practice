8cb6422c955f9494f643d5b514985e3fa45e9a52
Ankush Kedar <ankushkedar2007@gmail.com>
2026-09-12 22:48:19 +0530

    Time: 3 ms (42.63%), Space: 43 MB (71.31%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,17 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        List<Character> list = new ArrayList<>();
-
+        char result = 0;
         for(int i=0; i<s.length(); i++) {
-            list.add(s.charAt(i));
+            result = (char) (result ^ s.charAt(i));
         }
 
         for(int i=0; i<t.length(); i++) {
-            if(!list.contains(t.charAt(i))) {
-                return t.charAt(i);
-            }
-            list.remove((Character) t.charAt(i));
+            result = (char) (result ^ t.charAt(i));
         }
-        return '\0';
+        return result;
     }
 }
